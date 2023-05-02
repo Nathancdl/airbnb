4fa5e2fe4ca393e6515a39ac80d0dcb87efe2178
Elias Merhrioui <54977513+EliasMerhrioui@users.noreply.github.com>
2023-05-02 12:43:09 +0200

    add base de données

---
+++ src/Entity/Paiements.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PaiementRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PaiementRepository::class)
+ */
+class Paiement
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Logement::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $logement;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $montant;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $datePaiement;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getLogement(): ?Logement
+    {
+        return $this->logement;
+    }
+
+    public function setLogement(?Logement $logement): self
+    {
+        $this->logement = $logement;
+
+        return $this;
+    }
+
+    public function getMontant(): ?int
+    {
+        return $this->montant;
+    }
+
+    public function setMontant(int $montant): self
+    {
+        $this->montant = $montant;
+
+        return $this;
+    }
+
+    public function getDatePaiement(): ?\DateTimeInterface
+    {
+        return $this->datePaiement;
+    }
+
+    public function setDatePaiement(\DateTimeInterface $datePaiement): self
+    {
+        $this->datePaiement = $datePaiement;
+
+        return $this;
+    }
+}
