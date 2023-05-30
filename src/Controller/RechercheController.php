9b4d5aeb3685b5ad597563e452af08061ace41c3
alexmi94 <alexandrecance94@gmail.com>
2023-05-30 17:41:23 +0200

    add use forms

--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Forms;
 
 class RechercheController extends AbstractController
 {
     #[Route('/search', name: 'app_recherche')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         return $this->render('recherche/index.html.twig', [
             'controller_name' => 'RechercheController',
