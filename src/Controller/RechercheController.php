d043c18a7ad6882b2cec51467389e9c93030871c
alexmi94 <alexandrecance94@gmail.com>
2023-06-27 11:09:45 +0200

    recherche

--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -2,19 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Logement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Forms;
+use App\Form\RechercheType;
+use Doctrine\ORM\EntityManagerInterface;
 
 class RechercheController extends AbstractController
 {
+    private $entityManager;
+
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
     #[Route('/search', name: 'app_recherche')]
     public function index(Request $request): Response
     {
+        $form = $this->createForm(RechercheType::class);
+        $form->handleRequest($request);
+
+        $logements = [];
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ville = $form->getData()['ville'];
+            $logements = $this->entityManager->getRepository(Logement::class)->findBy(['ville' => $ville]);
+        }
+
         return $this->render('recherche/index.html.twig', [
-            'controller_name' => 'RechercheController',
+            'form' => $form,
+            'logements' => $logements
         ]);
     }
 }
--- src/Form/RechercheType.php
+++ src/Form/RechercheType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RechercheType extends AbstractType
 {
@@ -14,9 +15,6 @@ class RechercheType extends AbstractType
             ->add('ville', TextType::class, [
                 'label' => 'Ville',
                 'required' => true,
-            ])
-            ->add('rechercher', SubmitType::class, [
-                'label' => 'Rechercher',
             ]);
     }
 
